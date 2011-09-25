a31244a23ed61abcae43163bbe61896124d1c1d8
luc4 <lucagraf@gmx.de>
2011-09-25 11:37:38 +0200

    finished 'duplicate key' implementation

--- src/test/java/org/test/timer/ArquillianTest.java
+++ src/test/java/org/test/timer/ArquillianTest.java
@@ -1,7 +1,3 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.test.timer;
 
 import javax.ejb.EJB;
@@ -9,66 +5,82 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
 import java.util.HashSet;
-import org.junit.Before;
 import org.test.entity.Child;
+import org.test.service.IndependentEntityService;
 import org.test.service.ParentService;
-import java.io.IOException;
+
 import org.jboss.arquillian.container.test.api.Deployment;
-import java.io.Serializable;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.runner.RunWith;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
 import org.test.entity.EntityRelation;
+import org.test.entity.IndependentEntity;
 import org.test.entity.Parent;
 import static org.junit.Assert.*;
 
 
 /**
- *
  * @author sbn
  */
 @RunWith( Arquillian.class )
-public class ArquillianTest implements Serializable
+public class ArquillianTest
 {
 	
 	@EJB
 	private ParentService ps;
 	
+	@EJB
+	private IndependentEntityService is;
+	
+	
 	@Deployment
-	public static WebArchive createDeploymentPackage() throws IOException
+	public static WebArchive createDeploymentPackage() throws Exception
 	{
-		WebArchive war = ShrinkWrap.create( WebArchive.class, "test.war" ).
-				addPackage( Child.class.getPackage() ).
+		return ShrinkWrap.create( WebArchive.class, "test.war" ).
+				addPackage( Parent.class.getPackage() ).
 				addPackage( ParentService.class.getPackage() ).
-				addPackage( Runner.class.getPackage() ).addAsResource(
-				"persistence.xml", "META-INF/persistence.xml" );
-		return war;
+				addAsResource( "persistence.xml", "META-INF/persistence.xml" );
 	}
 	
 	
-	@Before
-	public void setUpDB()
+	@Test
+	public void forceDuplicateKeyException() throws Exception
 	{
-		List<EntityRelation> ers = new ArrayList();
+		setUpDB();
 		
-		EntityRelation er1 = new EntityRelation();
-		EntityRelation er2 = new EntityRelation();
-		EntityRelation er3 = new EntityRelation();
-		ps.persist( er1 );
-		ps.persist( er2 );
-		ps.persist( er3 );
-		ers.add( er1 );
-		ers.add( er2 );
-		ers.add( er3 );
+		List<Parent> parents = ps.getAllParents();
+		assertEquals( 2, parents.size() );
+		for( Parent parent : parents )
+		{
+			assertEquals( 3, parent.getChilds().size() );
+			IndependentEntity ie = is.selectOrCreate( -1L );
+			assertNull( ie.getId() );
+			for( Child child : parent.getChilds() )
+			{
+				assertNotNull( child.getEntityRelation() );
+			}
+			ie = is.merge( ie );
+			assertNotNull( ie.getId() );
+		}			
+	}
+	
+	
+	private void setUpDB() throws Exception
+	{
+		List<EntityRelation> ers = new ArrayList<EntityRelation>();
+		for( int i=0; i<3; i++ )
+		{
+			EntityRelation er = new EntityRelation();
+			ps.persist( er );
+			ers.add( er );
+		}
 		
-		
-		
-		for( int i = 1; i < 3; i++ )
+		for( int i=0; i<2; i++ )
 		{
 			Parent p = new Parent();
-			Set<Child> childs = new HashSet();
+			Set<Child> childs = new HashSet<Child>();
 			for( EntityRelation er : ers )
 			{
 				Child c = new Child();
@@ -78,18 +90,6 @@ public class ArquillianTest implements Serializable
 			p.setChildren( childs );
 			ps.persist( p );
 		}
-		
-
 	}
-	
-	
-	@Test
-	public void testImportGoodCoupons() throws Exception
-	{
-				
-		assertTrue( true );
-		Thread.sleep( 120000 );
-	}
-	
 	
 }
