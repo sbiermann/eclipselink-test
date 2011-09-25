27b7cd0a8494cc8f662c20148ccf7a743fdda2b0
luc4 <lucagraf@gmx.de>
2011-09-25 11:30:53 +0200

    renamed referencedd services and vars in Runner

--- src/main/java/org/test/timer/Runner.java
+++ src/main/java/org/test/timer/Runner.java
@@ -24,22 +24,22 @@ public class Runner
 	private static final Logger log = LoggerFactory.getLogger( Runner.class );
 	
 	@EJB
-	private ParentService cs;
+	private ParentService ps;
 
 	@EJB
-	private IndependentEntityService ls;
+	private IndependentEntityService is;
 	
 
 	@Schedule( hour="*", minute="*" )
 	public void run()
 	{
-		List<Parent> parents = cs.getAllParents();
+		List<Parent> parents = ps.getAllParents();
 		log.debug( "{} parents found.", parents.size() );
 		for( Parent parent : parents )
 		{
 			log.debug( "Current parent: {} ...", parent.getId() );
 			log.debug( "Select or create IndependetEntity ..." );
-			IndependentEntity clog = ls.selectOrCreate( -1L );
+			IndependentEntity ie = is.selectOrCreate( -1L );
 			log.debug( "Get childs for parent: {} ...", parent.getId() );
 			log.debug( "{} childs for parent: {} found.", parent.getChilds().size(), parent.getId() );
 			for( Child child : parent.getChilds() )
@@ -48,7 +48,7 @@ public class Runner
 				log.debug( "Get EntityRelation: {}", child.getEntityRelation() );
 			}
 			log.debug( "Merge IndependentEntity ..." );
-			ls.merge( clog );
+			is.merge( ie );
 		}
 	}
 
